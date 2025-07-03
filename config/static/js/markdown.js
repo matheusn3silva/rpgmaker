1aaf584ec1ca3d6895cb131ca30d9c48c855a61d
Matheus (Manasi) <matheusnascimentox725@gmail.com>
2025-07-03 19:46:41 -0300

    Tentando colocar o div vazio no preview do markdown

--- config/static/js/markdown.js
+++ config/static/js/markdown.js
@@ -1,7 +1,17 @@
 const textarea = document.getElementById('history-character');
 const preview = document.getElementById('preview-markdown');
+const empty = document.getElementById('empty');
+
+function verifyPreviewContent(valueInput) {
+    if (valueInput.trim() != '') {
+        empty.style.display = 'none'
+    } else {
+        empty.style.display = 'flex'
+    }
+}
 
 textarea.addEventListener('input', () => {
     const markdown = textarea.value
     preview.innerHTML = marked.parse(markdown)
+    verifyPreviewContent(markdown)
 })
